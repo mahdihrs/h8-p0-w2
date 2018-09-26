e07c16c3004b284808578fcd21ecfef0d874461d
Mahdihrs <mahdiharis@outlook.com>
2018-09-26 17:36:33 +0700

    Perbaikan indentasi dan test case

--- [Exercises 12] Konversi Menit.js
+++ [Exercises 12] Konversi Menit.js
@@ -3,22 +3,20 @@ function konversiMenit(menit) {
       var hour = menit / 60;
       var mins = "00";
       return hour + ":" + mins;
-    }
+  }
   else if (menit % 60 !== 0) {
       var hour = Math.floor(menit/60);
       var mins = menit % 60;
-         if (mins < 10) {
-           mins = "0" + mins;
-         }
-         else {
-           mins;
-         }
+      if (mins < 10) {
+        mins = "0" + mins;
+      }
       return hour + ":" + mins;
     }
-  else {
-      return "Waktu salah";
-    }
-    return hour,mins;
   }
 
-console.log(konversiMenit(70));
+//TEST CASE
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
